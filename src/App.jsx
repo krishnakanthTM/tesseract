967045a7c8cb49898442095624f2716e8de1a9bb
Suryanarayana Yallamelli <surya@localhost.localdomain>
2023-09-29 22:36:17 +0530

    Added Routing and Created Solutions, Pricing and Products Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./App/Pages/Home/HomePage";
 import Products from "./App/Pages/Products/Products"
 import Solutions from "./App/Pages/Solutions/Solutions"
-import PricingPage from "./App/Components/PricingPage/PricingPage";
-// import Pricing from "./App/Pages/Pricing/Pricing"
+import Pricing from "./App/Pages/Pricing/Pricing"
 
 function App() {
     return ( 
@@ -15,8 +14,7 @@ function App() {
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/Products" element={<Products/>}/>
                 <Route path="/Solutions" element={<Solutions/>}/>
-                <Route path="/PricingPage" element={<PricingPage/>}/>
-                {/* <Route path="/Pricing" element={<Pricing/>}/> */}
+                <Route path="/Pricing" element={<Pricing/>}/>
             </Routes>
         </BrowserRouter>
     </>
